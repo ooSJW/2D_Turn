e86098be65aae70eb81b4948e3ad1c9a7172d67a
ooSJW <tjwjddn1927@gmail.com>
2025-08-07 16:55:09 +0900

    Refactor : 스킬 UI_Update -> Coroutine으로 변경

--- Assets/0_Scripts/3_Object/4_UI/SkillButton/SkillButton.cs
+++ Assets/0_Scripts/3_Object/4_UI/SkillButton/SkillButton.cs
@@ -22,7 +22,12 @@ namespace project02
             set
             {
                 isCooltime = value;
-                if (IsCoolTime)
+                /*
+                    owner 캐릭터가 스킬을 사용할 때 스킬 객체의 IsCoolDown = false로 변함.
+                    스킬 객체의 IsCoolDown프로퍼티에서 해당 프로퍼티의 값을 바꾸는 방식
+                    캐릭터 -> 스킬 객체 -> UI 순서로 메시지가 전달되는 방식으로 구현.
+                */
+                if (isCooltime)
                 {
                     skillButton.interactable = false;
                     hideImage.gameObject.SetActive(true);
@@ -33,6 +38,8 @@ namespace project02
                 {
                     skillButton.interactable = true;
                     hideImage.gameObject.SetActive(false);
+
+                    // 코루틴 내부의 예기치 못한 무한루프를 방지하기 위해 한번 더 Stop하는 코드를 작성.
                     if (cooldownRoutine is not null)
                         StopCoroutine(cooldownRoutine);
 
@@ -52,6 +59,8 @@ namespace project02
                     isSelected = value;
                     if (isSelected)
                     {
+                        // 해당 스킬이 선택되었을 때 Owner객체의 공격, 공격 대기 등의 기능을 직접 호출하는 것이 아닌
+                        // 행동 대기 list에 자신의 이름만을 추가.
                         owner.selectedSkillList.Add(skillName);
                         SetSkillImageColor(0.5f);
                     }
@@ -102,6 +111,9 @@ namespace project02
             // 스킬 객체의 정보를 통해 이미지, 쿨타임 등의 정보가 동적으로 정해짐.
             skillBase = skillBaseValue;
             skillBase.SkillButton = this;
+            skillIcon.sprite = Resources.Load<Sprite>(Path_SkillIcon + skillBase.SkillInfo.skill_icon);
+            coolTime = skillBase.SkillInfo.cool_time;
+
             owner = MainSystem.Instance.PlayerManager.Player.
                 activeKnightList.Find(elem => elem.KnightStatInformation.name == skillBase.SkillInfo.owner);
             Allocate();
